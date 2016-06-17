199b8f42eabc3da9331b5badfdf9abb82f7b43c3
Boris Shirvinsky <bshirvinsky@ea.com>
2016-06-17 20:14:09 +0300

    lol

--- big_integer/big_integer/big_integer.h
+++ big_integer/big_integer/big_integer.h
@@ -12,7 +12,6 @@ public:
 	big_integer(big_integer const &other);
 	~big_integer();
 
-
 	big_integer &operator=(big_integer const &other);
 	big_integer &operator+=(big_integer const &rhs);
 	big_integer &operator-=(big_integer const &rhs);
