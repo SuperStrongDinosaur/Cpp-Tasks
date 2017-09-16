88fbed9c5b7634a0e597381527823a65350dc06a
Alex Shirvinsky <alex_shirv@mail.ru>
2017-09-16 15:56:49 +0300

    Update memcpy.cpp

--- cpp-tasks/Assembler/memcpy.cpp
+++ cpp-tasks/Assembler/memcpy.cpp
@@ -1,6 +1,7 @@
 #include <ctime>
 #include <iostream>
 #include <emmintrin.h>
+#include <vector>
 
 const size_t N = (1 << 30);
 
@@ -32,29 +33,31 @@ void memcpy_asm(void *to, const void *from, size_t sz) {
     _mm_sfence();
 }
 
-bool check(void const* a, void const* b, size_t size) {
-    for(size_t i = 0; i < size; i++)
-        if(*((char*) a + i) != *((char*) b + i))
+bool check(std::vector<char> & a, std::vector<char> & b) {
+    for(size_t i = 0; i < a.size(); i++)
+        if(a[i] != b[i])
             return false;
     return true;
 }
 
 int main() {
-    char *from = new char[N];
-    char *to = new char[N];
+    //char *from = new char[N];
+   // char *to = new char[N];
+    std::vector<char> from(N);
+    std::vector<char> to(N);
     
+    for(;;) {
     auto begin = std::clock();
-    memcpy_simple(to, from, N);
+    memcpy_simple(to.data(), from.data(), N);
     std::cout << (double)(std::clock() - begin) / CLOCKS_PER_SEC << std::endl;
-    if(!check(from, to, N))
+    if(!check(from, to))
         std::cout << "Memory isn't equal" << ::std::endl;
     
-    from = new char[N];
-    to = new char[N];
     begin = std::clock();
-    memcpy_asm(to, from, N);
+    memcpy_asm(to.data(), from.data(), N);
     std::cout << (double)(std::clock() - begin) / CLOCKS_PER_SEC << std::endl;
-    if(!check(from, to, N))
+    if(!check(from, to))
         std::cout << "Memory isn't equal" << ::std::endl;
+    }
     return 0;
 }
